976ec21238c8ae8e10576ec04207b3924d4fb6e7
Ben Sprague <68568908+ben-sprague@users.noreply.github.com>
2026-07-30 13:02:45 -0400

    remove density calculation (use gsw built in function instead)

---
+++ ocean_tools.py
@@ -0,0 +1,61 @@
+'''
+Functions to calculate various basic parameters in/related to the ocean
+'''
+
+import numpy as np
+import gsw
+
+def fc(latitude: np.ndarray) -> np.ndarray:
+    '''
+    Calculature the coriolis parameter (f_c) on earth
+
+    Parameters:
+    ----------
+    latitude: the latitude to calculate f_c at in degrees (+ North, - South)
+
+    Returns:
+    ----------
+    Coriolis parameter
+    '''
+
+    omega = 7.29e-5 #earth sidereal rotation rate (rad/s)
+
+    lat_radians = latitude*np.pi/180 #Convert latitude from degrees to radians for np.sin
+
+    return 2*omega*np.sin(lat_radians)
+
+def distance_from_datum(
+        latitude: np.ndarray,
+        longitude: np.ndarray,
+        datum: tuple
+        ) -> np.ndarray:
+    '''
+    Calculates the distance of various points from a datum assuming a spherical earth
+
+    Parameters
+    ----------
+    latitude : latitude (degrees)
+    longitude : longitude (degrees)
+    datum : latitude and longitude of reference datum
+
+    Returns
+    -------
+    distance : distance (in km) of each lat/lon pair from datum
+    '''
+
+    datum_lat = datum[0]
+    datum_lon = datum[1]
+
+    distance = np.full_like(latitude, np.nan)
+
+    for i in range (latitude.shape[0]):
+        current_lat = latitude[i]
+        current_lon = longitude[i]
+        distance[i] = gsw.distance([datum_lon, current_lon], [datum_lat, current_lat])[0]
+
+
+    distance = distance/1000 #Convert from m to km
+
+    return distance
+
+
