fcf5d09261f5a1449c7a175ccbc732333593eb3b
Ben Sprague <68568908+ben-sprague@users.noreply.github.com>
2026-08-21 12:53:30 -0400

    Add function to zip processed datafile diconaries back into one netCDF file

--- datatools/io/writers.py
+++ datatools/io/writers.py
@@ -1,8 +1,20 @@
 import xarray as xr
 from datetime import datetime
 
+from ..ctd.operations import zip_by_coordinate
+
 def save_netCDF(ds: xr.Dataset, path: str):
     ds.attrs['processed_date'] = datetime.now().isoformat()
     ds.attrs['processed_by'] = 'Ben Sprague'
 
     ds.to_netcdf(path, engine = "h5netcdf")
+
+
+def save_netCDF_from_dict(da_dict: dict, path: str):
+    ds = zip_by_coordinate(da_dict, 'cruise')
+    ds.attrs['processed_date'] = datetime.now().isoformat()
+    ds.attrs['processed_by'] = 'Ben Sprague'
+
+    ds.to_netcdf(path, engine = "h5netcdf")
+
+    
